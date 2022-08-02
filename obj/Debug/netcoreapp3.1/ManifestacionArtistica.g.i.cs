81408277ba94fd1974ed9f9a441020a4dbd7c9ac
Nayhan Provedo Labrada <nayhanprovedolabrada@gmail.com>
2022-08-02 02:43:08 -0400

    agregar cambios

--- obj/Debug/netcoreapp3.1/ManifestacionArtistica.g.i.cs
+++ obj/Debug/netcoreapp3.1/ManifestacionArtistica.g.i.cs
@@ -69,7 +69,7 @@ namespace ApoloAdmin {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/ApoloAdmin;component/manifestacionartistica.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/ApoloAdmin;V1.0.0.0;component/manifestacionartistica.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\ManifestacionArtistica.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
